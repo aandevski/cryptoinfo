48f29e4333ed0fc54441475c294e397055a9bc8e
Unknown <mariovelickovski@gmail.com>
2018-08-24 20:30:48 +0200

    Import Bootstrap, implement Bootstrap navbar
    - Import Bootstrap v3 from CDN in index.html
    - Implement Bootstrap navbar in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import {Navbar, Nav} from 'react-bootstrap'
+
 export class Header extends Component {
 
 	render() {
 		return (
-			<div style={{background: '#DDD'}}>
-				<Link to="/">HOMEPAGE</Link>
-				<ul>
-					<li><Link to="/currencies">Currencies</Link></li>
-					<li><Link to="/rigs">Mining rigs</Link></li>
-					<li><Link to="/converter">Converter</Link></li>
-				</ul>
-			</div>
+			<Navbar inverse style={{borderRadius:0}}>
+			  <Navbar.Header>
+			    <Navbar.Brand>
+			      <Link to="/">CryptoInfo</Link>
+			    </Navbar.Brand>
+			  </Navbar.Header>
+			  <Nav>
+			  	<li><Link to="/currencies">Currencies</Link></li>
+			  	<li><Link to="/rigs">Mining rigs</Link></li>
+			  	<li><Link to="/converter">Converter</Link></li>
+			  </Nav>
+			</Navbar>
 		);
 	}
 
