07f6fb923d9882c30ff360f557b3d856e10c59e1
Aleksandar Andevski <a.andevski@gmail.com>
2018-08-25 13:33:45 +0200

    Add CurrencyList to homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 
+import { PageHeader } from 'react-bootstrap';
+import { CurrencyListContainer } from '../containers/CurrencyListContainer';
+
 export class Homepage extends Component {
 
 	render() {
-		return <div><h1>Homepage goes here</h1></div>;
+		return (
+			<div>
+				<PageHeader>
+					Most popular currencies
+				</PageHeader>
+		    	<CurrencyListContainer limit={3}/>
+		    	<PageHeader>
+					Most popular mining rigs
+				</PageHeader>
+		    </div>
+		);
 	}
 
 }
