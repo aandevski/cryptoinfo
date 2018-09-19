e381dc465e12823734a41952736e28a22acde0d3
vmario42 <mvmarvel9@gmail.com>
2018-09-19 19:57:13 +0200

    Add mockup code to converter

--- src/components/pages/Converter.js
+++ src/components/pages/Converter.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
 
+import {FormControl, FormGroup, Row, Col} from 'react-bootstrap'
+
 export class ConverterPage extends Component {
 
 	render() {
-		return <div><h1>Converter goes here</h1></div>;
+		return (
+			<div>
+				<h1>Converter</h1>
+
+				<form>
+					<FormGroup>
+
+						<Row>
+							<Col xs="6" md="3">
+								<FormControl
+									type="number"
+								/>
+							</Col>
+							<Col xs="6" md="3">
+								<FormControl componentClass="select" placeholder="select">
+									<option value="btc">BTC</option>
+									<option value="usd">USD</option>
+								</FormControl>
+							</Col>
+							<Col xs="6" md="3">
+								<FormControl
+									type="number"
+								/>
+							</Col>
+							<Col xs="6" md="3">
+								<FormControl componentClass="select" placeholder="select">
+									<option value="btc">BTC</option>
+									<option value="usd">USD</option>
+								</FormControl>
+							</Col>
+						</Row>
+					</FormGroup>
+				</form>
+			</div>
+		);
 	}
 
 }
