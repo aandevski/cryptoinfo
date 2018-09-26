b593388b9a2bdb42b778efd588a22171b8458263
vmario42 <mvmarvel9@gmail.com>
2018-09-26 16:25:29 +0200

    Change link in Currency Row

--- src/components/CurrencyRow.js
+++ src/components/CurrencyRow.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import config from '../config';
 
+import {Link} from 'react-router-dom'
+
 export class CurrencyRow extends Component {
 	constructor(props) {
 		super(props)
-		this.handleClick = this.handleClick.bind(this)
-	}
-	handleClick() {
-		window.location.href = '/currencies/' + this.props.details.Name
 	}
 	render() {
 		return (
-			<tr onClick={this.handleClick}>
+			<tr>
 				<td>
 					<img
 						src={config.baseUrl + this.props.details.ImageUrl}
 						style={{width: 50}}
 					/>
 				</td>
-				<td>{this.props.details.Name}</td>
+				<td><Link to={"currencies/"+this.props.details.Name}>{this.props.details.Name}</Link></td>
 				<td>{this.props.details.FullName}</td>
 				<td>$ {(parseFloat(this.props.conversionInfo.TotalVolume24H)*this.props.details.price).toLocaleString()}</td>
 				<td>$ {(this.props.conversionInfo.Supply*this.props.details.price).toLocaleString()}</td>
